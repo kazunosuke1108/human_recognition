475f3624e8e3fb7c803efb32fae6117d178414a4
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-08-27 18:37:50 +0900

    ADD: env for opencv completed

---
+++ docker/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+# xhost +
+xhost +local:user
+# xhost + 192.168.1.54
+nvidia-docker &> /dev/null
+if [ $? -ne 0 ]; then
+    echo $TAG
+    echo "=============================" 
+    echo "=nvidia docker not installed="
+    echo "============================="
+else
+    echo "=========================" 
+    echo "=nvidia docker installed="
+    echo "========================="
+    docker run -it \
+    --privileged \
+    --runtime=nvidia \
+    --env=DISPLAY=$DISPLAY \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v "/home/${USER}/.Xauthority:/home/${USER}/.Xauthority" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --rm \
+    -v "/$(pwd)/global_ros_setting.sh:/ros_setting.sh" \
+    -v "/$(pwd)/ros_workspace:/home/${USER}/catkin_ws/" \
+    -v "${PWD}/config/terminator_config:/home/${USER}/.config/terminator/config" \
+    -v "/$(pwd)/../recog_opencv:/home/${USER}/catkin_ws/src/recog_opencv" \
+    -v /etc/group:/etc/group:ro \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /media:/media \
+    -v /dev:/dev \
+    --net host \
+    ${USER}/whill
+fi
---
+++ recog_opencv/scripts/test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import cv2
+
+# VideoCapture オブジェクトを取得します
+capture = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = capture.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
