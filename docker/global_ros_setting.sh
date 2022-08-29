18f1100316b7ee9cce1a2238aef812fbd05f7c0d
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-08-29 18:21:00 +0900

    FIX: Error around Yolo & numpy

--- docker/global_ros_setting.sh
+++ docker/global_ros_setting.sh
@@ -64,8 +64,8 @@ alias whill_mode='export ROS_MASTER_URI=http://${whill_ip}:11311 export PS1="\[\
 alias dlbox_mode='export ROS_MASTER_URI=http://${dlbox_ip}:11311 export PS1="\[\033[41;1;37m\]<DOCKER DLBOX_MODE>\[\033[0m\]\w$ "&& echo "ROS_MASTER_URI:"$ROS_MASTER_URI'
 
 # chmod a+x ~/catkin_ws/src/ytlab_navigation/ytlab_navigation_msgs/cfg/DWACostParams.cfg
-# sudo mkdir -p ~/.config/Ultralytics
-# sudo mkdir -p ~/.config/matplotlib
+sudo mkdir -p ~/.config/Ultralytics
+sudo mkdir -p ~/.config/matplotlib
 
 ## echo
 echo "ROS_IP:"$ROS_IP
