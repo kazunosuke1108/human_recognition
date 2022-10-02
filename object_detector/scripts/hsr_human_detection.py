10a0292123c7c39b2b7641fb42a14de59c406512
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-10-02 15:01:21 +0900

    WIP: kakurenbo slow

--- object_detector/scripts/hsr_human_detection.py
+++ object_detector/scripts/hsr_human_detection.py
@@ -50,9 +50,7 @@ ImageCallbackでやること
 dpt_align=True
 topic_name_rgb = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
 topic_name_dpt = '/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw'
-
-print(os.getcwd())
-
+topic_name_inf = '/hsrb/head_rgbd_sensor/depth_registered/camera_info'
 
 class Human_tracking():
     def __init__(self):
@@ -91,14 +89,14 @@ class Human_tracking():
     def pub_sub(self):
         sub_list=[]
         # subscriber
-        rgb_sub=message_filters.Subscriber(self.camera_frame+"/camera/color/image_raw",Image)
+        topic_name_rgb = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
+        topic_name_dpt = '/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw'
+        topic_name_inf = '/hsrb/head_rgbd_sensor/depth_registered/camera_info'
+        rgb_sub=message_filters.Subscriber(topic_name_rgb,Image)
         sub_list.append(rgb_sub)
-        if dpt_align:
-            dpt_sub=message_filters.Subscriber(self.camera_frame+"/camera/aligned_depth_to_color/image_raw",Image)
-        else:
-            dpt_sub=message_filters.Subscriber(self.camera_frame+"/camera/depth/image_rect_raw",Image)
+        dpt_sub=message_filters.Subscriber(topic_name_dpt,Image)
         sub_list.append(dpt_sub)
-        info_sub=message_filters.Subscriber(self.camera_frame+"/camera/depth/camera_info",CameraInfo)
+        info_sub=message_filters.Subscriber(topic_name_inf,CameraInfo)
         sub_list.append(info_sub)
         mf=message_filters.ApproximateTimeSynchronizer(sub_list,100,0.5)
 
@@ -184,76 +182,83 @@ class Human_tracking():
             self.pub_PointStamped.publish(point)
 
     def point_translation(self,person):
-        point=(person[-3][2]/1000, person[-3][1]/1000, person[-3][0]/1000)
-        print("###### DEBUG ROI STARTS ######")
+        point=(person[-3][0]/1000, person[-3][1]/1000, person[-3][2]/1000)
         print(point)
-        self.tf_history.append(list(point))
-        np.savetxt(f'/home/hayashide_kazuyuki/catkin_ws/src/object_detector/csv/0904_point.csv',self.tf_history,delimiter=",")
+        # self.tf_history.append(list(point))
+        # np.savetxt(f'/home/hayashide_kazuyuki/catkin_ws/src/object_detector/csv/0904_point.csv',self.tf_history,delimiter=",")
 
-        print("###### DEBUG ROI ENDS ######")
         
         return point
 
-    def publish_Pose2D(self,x,y,theta):
-        pose=Pose2D()
-        pose.x,pose.y,pose.theta=x,y,theta
-        self.pub_Pose2D.publish(pose)
+    # def publish_Pose2D(self,x,y,theta):
+    #     pose=Pose2D()
+    #     pose.x,pose.y,pose.theta=x,y,theta
+    #     self.pub_Pose2D.publish(pose)
     
     def broadcast_human(self,rect_list):
         for person in rect_list:
             br_human=tf.TransformBroadcaster()
+            ls_human=tf.TransformListener()
             # trans=self.point_translation(person)
-            trans=(self.point_translation(person)[0],0,self.point_translation(person)[2])
+            trans=(self.point_translation(person)[0],self.point_translation(person)[1],self.point_translation(person)[2])
             rot=(0,0,0,1)
+            R = rospy.Rate(150)
             try:
                 # br_human.sendTransform(trans,rot,rospy.Time.now(),"/human",self.camera_frame)
-                br_human.sendTransform(trans,rot,rospy.Time.now(),"/human",self.camera_frame_debug)
+                print("###### DEBUG ROI STARTS ######")
+                br_human.sendTransform(trans,rot,rospy.Time.now(),"/human_position_link","/head_rgbd_sensor_link")
+                R.sleep()
+                listen=ls_human.lookupTransform("/head_rgbd_sensor_link","/human_position_link",rospy.Time(0))
+                print(listen)
+                print("###### DEBUG ROI ENDS ######")
+
+
                 # print(self.camera_frame)
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as error_human:
                 print("error in broadcast_human:",error_human)
                 pass
 
-    def broadcast_goal(self):
-        ## vector from base_link to human
-        try:
-            (trans,rot)=self.listener.lookupTransform("/base_link","/human",rospy.Time(0))
+    # def broadcast_goal(self):
+    #     ## vector from base_link to human
+    #     try:
+    #         (trans,rot)=self.listener.lookupTransform("/base_link","/human",rospy.Time(0))
 
-            ## goal pose
-            goal_dist=np.array(trans)/np.linalg.norm(trans)
-            goal_trans=tuple(np.array(trans)-goal_dist)
-            # goal_trans=tuple((goal_trans[0],goal_trans[1],0))
-            goal_rot=rot
+    #         ## goal pose
+    #         goal_dist=np.array(trans)/np.linalg.norm(trans)
+    #         goal_trans=tuple(np.array(trans)-goal_dist)
+    #         # goal_trans=tuple((goal_trans[0],goal_trans[1],0))
+    #         goal_rot=rot
 
-            ## publish goal pose
-            br=tf.TransformBroadcaster()
-            br.sendTransform(goal_trans,goal_rot,rospy.Time.now(),"/goal","/base_link")
+    #         ## publish goal pose
+    #         br=tf.TransformBroadcaster()
+    #         br.sendTransform(goal_trans,goal_rot,rospy.Time.now(),"/goal","/base_link")
             
-            # calculate theta
-            e = tf.transformations.euler_from_quaternion(goal_rot)
-            return goal_trans[0],goal_trans[1],e[2]
+    #         # calculate theta
+    #         e = tf.transformations.euler_from_quaternion(goal_rot)
+    #         return goal_trans[0],goal_trans[1],e[2]
 
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            return 0,0,0
+    #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+    #         return 0,0,0
 
-    def publish_goal(self):
-        try:
-            (trans,rot)=self.listener.lookupTransform("/base_link","/goal",rospy.Time(0)) # 実機のときはmap。publish確認のときはbase_link
-            e = tf.transformations.euler_from_quaternion(rot)
-            self.publish_Pose2D(trans[0],trans[1],e[2])
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as error_goal:
-            print(error_goal)
-            pass
+    # def publish_goal(self):
+    #     try:
+    #         (trans,rot)=self.listener.lookupTransform("/base_link","/goal",rospy.Time(0)) # 実機のときはmap。publish確認のときはbase_link
+    #         e = tf.transformations.euler_from_quaternion(rot)
+    #         self.publish_Pose2D(trans[0],trans[1],e[2])
+    #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as error_goal:
+    #         print(error_goal)
+    #         pass
 
-    def export_log(self,now,rect_list): # rect_listが一番嬉しい
-        def flatten(l):
-            for el in l:
-                if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
-                    yield from flatten(el)
-                else:
-                    yield el
+    # def export_log(self,now,rect_list): # rect_listが一番嬉しい
+    #     def flatten(l):
+    #         for el in l:
+    #             if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
+    #                 yield from flatten(el)
+    #             else:
+    #                 yield el
 
-        for person in rect_list:
-            self.history.append(list(flatten(person)))
+        # for person in rect_list:
+        #     self.history.append(list(flatten(person)))
         # np.savetxt(f'/home/hayashide_kazuyuki/catkin_ws/src/object_detector/csv/{self.save_name}.csv',self.history,delimiter=",")
         
     def ImageCallback(self,rgb_data,dpt_data,info_data):
@@ -261,58 +266,63 @@ class Human_tracking():
             # unpack arrays
             rgb_array = np.frombuffer(rgb_data.data, dtype=np.uint8).reshape(rgb_data.height, rgb_data.width, -1)
             rgb_array=np.nan_to_num(rgb_array)
-            rgb_array=cv2.cvtColor(rgb_array,cv2.COLOR_BGR2RGB)
+            # rgb_array=cv2.cvtColor(rgb_array,cv2.COLOR_BGR2RGB)
             dpt_array = np.frombuffer(dpt_data.data, dtype=np.uint16).reshape(dpt_data.height, dpt_data.width, -1)
             dpt_array=np.nan_to_num(dpt_array)
             Proj_mtx=np.array(info_data.P).reshape(3,4)
+            
 
             # rotation
-            if self.rotate_mode=='left_down':
-                rotation=cv2.ROTATE_90_COUNTERCLOCKWISE
-                rgb_array=cv2.rotate(rgb_array,rotation)
-                dpt_array=cv2.rotate(dpt_array,rotation)
-            elif self.rotate_mode=='right_down':
-                rotation=cv2.ROTATE_90_CLOCKWISE
-                rgb_array=cv2.rotate(rgb_array,rotation)
-                dpt_array=cv2.rotate(dpt_array,rotation)
-            else:
-                rotation=cv2.ROTATE_90_CLOCKWISE
-                rgb_array=cv2.rotate(rgb_array,rotation)
-                dpt_array=cv2.rotate(dpt_array,rotation)
-                pass
+            # if self.rotate_mode=='left_down':
+            #     rotation=cv2.ROTATE_90_COUNTERCLOCKWISE
+            #     rgb_array=cv2.rotate(rgb_array,rotation)
+            #     dpt_array=cv2.rotate(dpt_array,rotation)
+            # elif self.rotate_mode=='right_down':
+            #     rotation=cv2.ROTATE_90_CLOCKWISE
+            #     rgb_array=cv2.rotate(rgb_array,rotation)
+            #     dpt_array=cv2.rotate(dpt_array,rotation)
+            # else:
+            #     rotation=cv2.ROTATE_90_CLOCKWISE
+            #     rgb_array=cv2.rotate(rgb_array,rotation)
+            #     dpt_array=cv2.rotate(dpt_array,rotation)
+            #     pass
             
             # object recognition
             results=self.model(rgb_array)
             objects=results.pandas().xyxy[0]
             obj_people=objects[objects['name']=='person']
             rect_list=self.get_position(rgb_array,dpt_array,obj_people,Proj_mtx)
+            print(rect_list)
             now=rospy.get_time()-self.t_start # この時刻をplotする（複数人検出時に同じタイムスタンプになるように）
             
             # publish for visualization
-            for person in rect_list:
+            self.broadcast_human(rect_list)
+            # for person in rect_list:
                 # human position
-                person_x,person_y,person_z=self.point_translation(person)
-                person_theta=0
-                self.publish_Pose2D(person_x,person_y,person_theta)
+                # person_x,person_y,person_z=self.point_translation(person)
+                # person_theta=0
+                # self.publish_Pose2D(person_x,person_y,person_theta)
 
                 # goal position
-                goal_x,goal_y,goal_theta=self.broadcast_goal()
-                self.publish_Pose2D(goal_x,goal_y,goal_theta)
+                # goal_x,goal_y,goal_theta=self.broadcast_goal()
+                # self.publish_Pose2D(goal_x,goal_y,goal_theta)
             # self.export_log(now,rect_list)
 
             # calculate goal pose
-            self.broadcast_goal()
-            self.publish_goal()
-            self.broadcast_human(rect_list)
+            # self.broadcast_goal()
+            # self.publish_goal()
+            # self.broadcast_human(rect_list)
 
             # imshow
-            results.render()
-            cv2.imshow("detected",results.imgs[0])
-            dpt_array_show=(dpt_array-np.min(dpt_array))/(np.max(dpt_array)-np.min(dpt_array))*255
-            dpt_array_show=cv2.applyColorMap(np.uint8(dpt_array_show),cv2.COLORMAP_JET)
-            dpt_array_show=self.draw_rect(dpt_array_show,rect_list)
+            if int(rospy.get_time())%2==0:
+                results.render()
+                results.save(save_dir="static/")
+            # cv2.imshow("detected",results.imgs[0])
+            # dpt_array_show=(dpt_array-np.min(dpt_array))/(np.max(dpt_array)-np.min(dpt_array))*255
+            # dpt_array_show=cv2.applyColorMap(np.uint8(dpt_array_show),cv2.COLORMAP_JET)
+            # dpt_array_show=self.draw_rect(dpt_array_show,rect_list)
             # cv2.imshow("depth",dpt_array_show)
-            cv2.waitKey(1)
+            # cv2.waitKey(1)
 
 
         except Exception:
