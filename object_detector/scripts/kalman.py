a1db926a169f5718def6b0c84f1b770e5aac33cb
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-10-05 09:27:19 +0900

    Revert "WIP: yesterday's progress"
    This reverts commit c03494b77c1eca8d79d1a7f5cbd291b16de50fe7.

--- object_detector/scripts/kalman.py
+++ object_detector/scripts/kalman.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 dt=1/30
 A=np.array([
@@ -8,29 +7,29 @@ A=np.array([
     [0,1,0,dt],
     [0,0,1,0],
     [0,0,0,1]
-],dtype=np.float64)
+])
 P=np.array([
     [1,0,0,0],
     [0,1,0,0],
     [0,0,1,0],
     [0,0,0,1]
-],dtype=np.float64)
+])
 W=np.array([
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0]
-],dtype=np.float64)
+])
 V=np.array([
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0]
-],dtype=np.float64)
+])
 
 for i in range(1,3):
-    print(f"###### SEQUENCE {i-2} #####")
-    P=np.dot(np.dot(A,P),np.linalg.pinv(A))-np.dot(np.dot(np.dot(np.dot(A,P),np.linalg.pinv(P+V)),P),A.T)+W
+    print(f"###### SEQUENCE {i} #####")
+    P=np.dot(np.dot(A,P),np.linalg.pinv(A))-np.dot(np.dot(np.dot(np.dot(A,P),np.linalg.pinv(P+V)),P),A)+W
     H=np.dot(np.dot(A,P),np.linalg.pinv(P+V))
     print("P",P)
     print("H",H)
@@ -40,48 +39,22 @@ csv_data=np.loadtxt(csv_path,delimiter=",")
 csv_data_pd=pd.DataFrame(csv_data).to_numpy()
 print(csv_data_pd)
 
-data_old=np.zeros_like(csv_data_pd[0])
-comp_flg=False
-history=[]
+data_old=None
+x=np.array([
+        [data[1]],
+        [data[2]],
+        [data_old[1]*(1+dt)],
+        [data_old[1]*(1+dt)],
+
+])
 for i,data in enumerate(csv_data_pd):
-    try:
-        data_next=csv_data_pd[i+1]
-    except IndexError:
-        break
-    
-    if comp_flg:
-        comp_flg=False
-        pass
-    else:
-        X=np.array([
-                data[1],
-                data[2],
-                (data[1]-data_old[1])/dt,
-                (data[2]-data_old[2])/dt,
-        ],dtype=np.float64).T
-    
-    if i==0:
-        Xhat=X
-    
-    if np.isnan(data_next[1]) or np.isnan(data[1]) or np.isnan(data_old[1]):
-        print(f"######SEQUENCE {i} X[{i}] -> X[{i+1}] #####")
-        P=np.dot(np.dot(A,P),np.linalg.pinv(A))-np.dot(np.dot(np.dot(np.dot(A,P),np.linalg.pinv(P+V)),P),A.T)+W
-        H=np.dot(np.dot(A,P),np.linalg.pinv(P+V))
-        print("P",P)
-        print("H",H)
-        print("X",X)
-        Xhat=np.dot(A,Xhat)+np.dot(H,X-Xhat)
-        print("Xhat",Xhat)
-        X=Xhat
-        comp_flg=True
-
-
-        data_old=data
-    history.append([X[0],X[1]])
+    if np.isnan(data[2]):
+        print(f"###### SEQUENCE {i+2} #####")
+        x=np.array([
+            [data[1]],
+            [data[2]],
+            [data_old[1]*(1+dt)],
+            [data_old[1]*(1+dt)],
+        ])
+    data_old=data
     pass
-history=np.array(history)
-print(np.array(history)[:,0])
-plt.plot(history[:,0],history[:,1],label="kalman")
-plt.plot(csv_data_pd[:,1],csv_data_pd[:,2],label="raw")
-plt.legend()
-plt.show()
