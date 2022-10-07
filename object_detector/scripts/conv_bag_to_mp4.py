c969fd597b4cb18431509a0438a505869c753c05
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-10-07 22:24:24 +0900

    ADD: auto encoder from .bag -> .avi

---
+++ object_detector/scripts/conv_bag_to_mp4.py
@@ -0,0 +1,12 @@
+import os
+import subprocess as sp
+from glob import glob
+
+bags=sorted(glob("/media/hayashide/KIOXIA/hayashide/rosbag/*"))
+avi_path="/home/hayashide/catkin_ws/src/object_detector/scripts/temp/sources/"
+
+for bag in bags:
+    bag_basename=os.path.basename(bag)
+    cmd=f"roslaunch object_detector conv_bag_to_avi.launch bag_path:={bag} save_path:={avi_path}{bag_basename[:-4]}.avi"
+    runcmd=sp.call(cmd.split())
+    print(runcmd)
