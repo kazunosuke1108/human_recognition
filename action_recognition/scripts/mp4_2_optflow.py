d24e2cbdcaa7b04e65524f065028e5bf3a681460
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-04-24 16:49:23 +0900

    go 2021b

--- action_recognition/scripts/mp4_2_optflow.py
+++ action_recognition/scripts/mp4_2_optflow.py
@@ -4,44 +4,44 @@ from glob import glob
 import cv2
 
 
-videoDirPath="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/EGTEA/video_clips"
-videoDirs=sorted(glob(videoDirPath+"/*/*"))
-# print(videoDirs)
-for videoDir in videoDirs:
-    # print(videoDir)
-    videos=sorted(glob(videoDir+"/*"))
-    # print(videos)
-    for videoPath in videos:
-        print(os.path.basename(videoPath))
-        cap=cv2.VideoCapture(videoPath)
-        ret, frame1 = cap.read()
-        prvsImg = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-        num_frame=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        # if "P05-R01-PastaSalad-1087378-1093795-F026082-F026267" in videoPath:
-        for i in range(num_frame):
-            saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]
-            os.makedirs(saveDir,exist_ok=True)
-            saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/u"
-            os.makedirs(saveDir,exist_ok=True)
-            saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/v"
-            os.makedirs(saveDir,exist_ok=True)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
-            ret, frame2 = cap.read()
-            nextImg = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(prvsImg,nextImg, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            # fimgu=cv2.normalize(flow[:,:,0],None,0,1e-7,cv2.NORM_MINMAX)
-            # fimgv=cv2.normalize(flow[:,:,1],None,0,1e-7,cv2.NORM_MINMAX)
-            fimgu=flow[:,:,0]*255/flow[:,:,0].max()
-            fimgv=flow[:,:,1]*255/flow[:,:,1].max()
-            # print(flow[:,:,0])
-            # ret, frame = cap.read()
-            # if ret:
-            resultPath_u="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/u/"+str(i+1).zfill(4)+".jpg"
-            resultPath_v="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/v/"+str(i+1).zfill(4)+".jpg"
-            # resultPath="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_rgb/"+os.path.basename(videoPath)[:-4]+"/"+str(i+1).zfill(4)+".jpg"
-            cv2.imwrite(resultPath_u, fimgu)
-            cv2.imwrite(resultPath_v, fimgv)
-            prvsImg = nextImg
+videoDirPath="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/EGTEA/video_clips/cropped_clips"
+videos=sorted(glob(videoDirPath+"/*/*"))
+print(len(videos))
+# for videoDir in videoDirs:
+#     # print(videoDir)
+#     videos=sorted(glob(videoDir+"/*"))
+#     # print(videos)
+for idx,videoPath in enumerate(videos[5379:]):
+    print(str(idx)+" / "+str(len(videos[5379:]))+"    "+os.path.basename(videoPath))
+    cap=cv2.VideoCapture(videoPath)
+    ret, frame1 = cap.read()
+    prvsImg = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    num_frame=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # if "P05-R01-PastaSalad-1087378-1093795-F026082-F026267" in videoPath:
+    for i in range(num_frame):
+        saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]
+        os.makedirs(saveDir,exist_ok=True)
+        saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/u"
+        os.makedirs(saveDir,exist_ok=True)
+        saveDir="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/v"
+        os.makedirs(saveDir,exist_ok=True)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
+        ret, frame2 = cap.read()
+        nextImg = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prvsImg,nextImg, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        # fimgu=cv2.normalize(flow[:,:,0],None,0,1e-7,cv2.NORM_MINMAX)
+        # fimgv=cv2.normalize(flow[:,:,1],None,0,1e-7,cv2.NORM_MINMAX)
+        fimgu=flow[:,:,0]*255/flow[:,:,0].max()
+        fimgv=flow[:,:,1]*255/flow[:,:,1].max()
+        # print(flow[:,:,0])
+        # ret, frame = cap.read()
+        # if ret:
+        resultPath_u="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/u/"+str(i+1).zfill(4)+".jpg"
+        resultPath_v="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_flow/"+os.path.basename(videoPath)[:-4]+"/v/"+str(i+1).zfill(4)+".jpg"
+        # resultPath="/home/hayashide/catkin_ws/src/third_party/Gaze-Attention/dataset/images_rgb/"+os.path.basename(videoPath)[:-4]+"/"+str(i+1).zfill(4)+".jpg"
+        cv2.imwrite(resultPath_u, fimgu)
+        cv2.imwrite(resultPath_v, fimgv)
+        prvsImg = nextImg
 
                 # cv2.imwrite(resultPath, frame)
                 # print(resultPath)
