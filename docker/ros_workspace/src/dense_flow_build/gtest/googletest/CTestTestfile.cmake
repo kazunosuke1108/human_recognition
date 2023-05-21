b82bea5f140c0e6438d4ca0468f24631cbd45173
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-05-22 08:27:55 +0900

    delete

--- docker/ros_workspace/src/dense_flow_build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/hayashide/catkin_ws/src/dense_flow_build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
